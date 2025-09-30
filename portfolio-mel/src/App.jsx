e199ac14ea785af2bf1556ff18419c8359561742
Madison Lisle <71103999+Madmaxbeyond@users.noreply.github.com>
2025-09-30 14:43:27 -0700

    Add button to temp portfolio

--- portfolio-mel/src/App.jsx
+++ portfolio-mel/src/App.jsx
@@ -6,11 +6,17 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://mlisle.wixsite.com/madisonlisle" target="_blank">
           <img src={mconLogo} className="logo" alt="Madison Lisle logo" />
         </a>
       </div>
       <h1>Madison Lisle <br></br> Frontend & Full-stack Software Engineer</h1>
+      <br></br>
+      <p className='secondary-txt'>My portfolio is currently under construction. To see my work & projects on a temporary site click the button below!</p>
+      <br></br>
+      <button>
+        <a href='https://mlisle.wixsite.com/madisonlisle'>Temporary Portfolio Site</a>
+      </button>
     </>
   )
 }
